c63ca7d3e3572bfd6153d3f665fee84ea2588035
davisricart <davisricart@gmail.com>
2025-05-24 12:19:00 -0400

    Update logo path to use relative URL

--- project/src/config/client.ts
+++ project/src/config/client.ts
@@ -11,7 +11,7 @@ export interface ClientConfig {
 const clientConfig: ClientConfig = {
   name: 'gr-balance',
   title: 'Beauty and Grace',
-  logo: 'https://github.com/davisricart/COPY_New_Customer/blob/main/public/images/gr-logo.png',
+  logo: '/images/gr-logo.png',
   theme: {
     primary: 'emerald',
     secondary: 'gray'
